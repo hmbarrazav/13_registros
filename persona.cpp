09b2a0561f55e060d5e0a2d11da6afc5673b9dab
hmbarrazav <hmbarrazav@gmail.com>
2025-06-24 08:16:41 -0500

    Ingreso de personas en persona.cpp

---
+++ persona.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct persona{
+    string nom;
+    int fn[3];
+};
+
+int main(){
+    int n, mes;
+    persona lista[100];
+    cout<<"Ingresa la cantidad de personas: "; cin>>n;
+    for(int i=0; i<n; i++){
+        cout<<"Persona #"<<i+1<<": "<<endl;
+        cout<<"Ingresa los nombres: ";
+        cin.ignore();
+        getline(cin, lista[i].nom);
+        cout<<"Ingresa fecha de nacimiento (dd mm aaaa): ";
+        cin>>lista[i].fn[0]>>lista[i].fn[1]>>lista[i].fn[2];
+    }
+
+    for(int i=0; i<n;i++){
+        cout<<lista[i].nom<<endl;
+        cout<<lista[i].fn[0]<<"/"<<lista[i].fn[1]<<"/"<<lista[i].fn[2]<<endl;
+        cout<<"---------------"<<endl;        
+    }
+    return 0;
+}
