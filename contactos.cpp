e714418050f6c303cf669d048abf9a7ad0943796
hmbarrazav <hmbarrazav@gmail.com>
2025-06-30 22:27:31 -0500

    Actualización de mensajes en contactos.cpp

--- contactos.cpp
+++ contactos.cpp
@@ -20,21 +20,21 @@ void imprimeContacto(contactoEmail &);
 int main(){
     int n, op;
     string nom, user, domain;
-    char sex;
+    char sex, SN;
     int edad;
     correo email;
     contactoEmail cont, lista[100];
     n = 0;
     do{
-        system("cls");
-        cout<<"Menu de opciones -------------------------"<<endl;
+        cout << "\033[2J\033[1;1H";
+        cout<<"Menu de opciones ::::::::::::::::::::::"<<endl;
         cout<<"1. Agregar contacto"<<endl;
         cout<<"2. Mostrar contactos"<<endl;
         cout<<"0. Salir"<<endl;
         cout<<"Elige una opcion: "; cin>>op;
+        cout<<endl;
         switch(op){
             case 1:
-                cout<<"Ingrese los datos de un usuario: "<<endl;
                 cin.ignore();
                 cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
                 cout<<"Ingrese el sexo (M/F): "; cin>>sex;
@@ -45,11 +45,19 @@ int main(){
                 
                 leerCorreo(email,user,domain);
                 leerContacto(cont,nom,sex,edad,email);
-                //imprimeContacto(cont);
 
-                lista[n] = cont;
-                n++;
-                system("pause");
+                cout<<"\nHa ingresado la siguiente informacion: "<<endl;
+                imprimeContacto(cont);
+                cout<<"\nEsta informacion es correcta? (S/N): ";
+                cin>>SN;
+                if(SN == 'S'){
+                    lista[n] = cont;
+                    n++;
+                    cout<<"Registro exitoso!"<<endl;
+                }
+                else{
+                    cout<<"Se cancelo la operacion"<<endl;
+                }
                 break;
             case 2:
                 for(int i = 0; i < n; i++){
@@ -57,16 +65,24 @@ int main(){
                     imprimeContacto(lista[i]);
                     cout<<endl;
                 }
-                system("pause");
                 break;
             case 0:
                 cout<<"Esta seguro de salir? (S/N): ";
+                cin>>SN;
+                if(SN != 'S'){
+                    op = -1;
+                    cout<<"Se cancelo la operacion"<<endl;
+                }
+                else
+                    cout<<"Hasta la proxima!"<<endl;
                 break;
             default:
-                cout<<"Opcion no valida!"<<endl;
-                system("pause");
+            cout<<"Opcion no valida!"<<endl;
                 break;
         }
+        cout<<"\nPresione una tecla para continuar...";
+        cin.ignore();
+        cin.get();
     } while(op != 0);
     return 0;
 }
